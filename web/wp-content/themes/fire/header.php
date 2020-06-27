23e708ed05a394bdc5a26b123626ca0f2557377f
John Young <john@skycatchfire.com>
2020-06-27 00:09:25 -0400

    adds nav

--- web/wp-content/themes/fire/header.php
+++ web/wp-content/themes/fire/header.php
@@ -13,47 +13,42 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'fire' ); ?></a>
+<div id="page" class="relative site">
+  <header x-data="" class="fixed top-0 w-full site-header">
+    <!-- <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> -->
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$fire_description = get_bloginfo( 'description', 'display' );
-			if ( $fire_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $fire_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <div class="container flex items-center justify-between my-4 text-white">
+      <a
+        :class="{ 'logo-active': isOpen }"
+        class="flex-shrink-0 w-24 text-white lg:w-40"
+        href="<?php echo esc_url( home_url( '/' ) ); ?>"
+        rel="home"
+      >
+        <?php include(get_template_directory() . '/assets/media/svgs/logo.svg'); ?>
+      </a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fire' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+      <nav class="" >
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location'  => 'menu-1',
+            'menu_id'         => 'primary-menu',
+            'menu_class'      => 'list-none flex justify-end items-center',
+            'list_item_class' => 'mx-4',
+            'link_class'      => 'no-underline uppercase text-white text-14 hover:underline transform scale-100',
+            )
+          );
+        ?>
+      </nav>
+    </div>
+
+  </header>
